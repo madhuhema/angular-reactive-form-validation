c90dab6846e88eac718ec4a3822d8199c435bd8d
madhuhema <madhunarmi@gmail.com>
2018-06-19 19:23:53 +0530

    reactive form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  application_form: FormGroup;
+  constructor() {
+    this.application_form = new FormGroup({
+      firstname : new FormControl('', Validators.required),
+      fullname : new FormControl('', Validators.required),
+      employeeCode : new FormControl('', Validators.pattern(/^[8]\d{6,6}/)),
+      bloodGroup: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.pattern('^[a-z|A-Z][a-z|A-Z|0-9|]+@virtusa.com')),
+    })
+  }
 }
